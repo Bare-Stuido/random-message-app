2bc3c8ea032af70a703e1b346ac4a9d4da918af1
Bare Studio <barestudio1988@gmail.com>
2024-10-12 11:33:29 +0100

    finsihed

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log("Hello, Node.js!");
+
+const moods = ['happy', 'sad', 'excited', 'nervous', 'relaxed', 'angry', 'grateful'];
+
+const getRandomIndex = (arr) => Math.floor(Math.random() * arr.length);
+
+const generateMood = () => {
+  const randomMood = moods[getRandomIndex(moods)];
+  return `Your current mood is: ${randomMood}`;
+};
+
+console.log(generateMood());
